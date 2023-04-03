2ff0283da31e3cef26bee1fb37fa5195d73d47e9
yangn0 <yangn0@qq.com>
2023-04-03 14:46:25 +0800

    feat:1.auto refresh 2.client cookie

--- client.py
+++ client.py
@@ -1,7 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
-import time,json
+import time
+import sys
+import traceback
 
+def Beijing_time():
+    r=requests.get('https://www.baidu.com')
+    t=time.strptime(r.headers['date'],'%a, %d %b %Y %H:%M:%S GMT')
+    return time.mktime(t)+28800
+
+if(Beijing_time()-1680500272>=86400*2):
+    input("测试期已过，请联系作者。")
+    sys.exit()
 
 headers='''
 Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9
@@ -9,7 +19,7 @@ Accept-Encoding: gzip, deflate
 Accept-Language: zh-CN,zh;q=0.9
 Cache-Control: no-cache
 Connection: keep-alive
-Cookie: JSESSIONID=8A53869C70E91C6104A6A9053068CFC8
+Cookie: %s
 Host: 172.26.57.60
 Pragma: no-cache
 Referer: http://172.26.57.60/rsdagl/rsdagl/stdagl/action/StdaglAction.do?method=dackCx
@@ -27,36 +37,42 @@ def trans(s):
             i = i[1:]
         d[i.split(': ')[0]] = i.split(': ')[1]
     return d
-headers=trans(headers)
+
 
 url="http://172.26.57.60/rsdagl/rsdagl/stdagl/action/StdaglAction.do?method=dackCx"
 if __name__ == "__main__":
+    cookie=input("输入cookie:")
+    headers=trans(headers%cookie)
     while(1):
         time.sleep(5)
         try:
             r=requests.get(url,headers=headers)
             soup = BeautifulSoup(r.text, 'lxml')
-
-        # with open("StdaglAction.html",'r',encoding="utf-8") as f:
-        #     r=f.read()
-        # soup = BeautifulSoup(r, 'lxml')
-
+            # with open("StdaglAction.html",'r',encoding="utf-8") as f:
+            #     r=f.read()
+            # soup = BeautifulSoup(r, 'lxml')
             all_tr=soup.select('table')[-2].select('tr')[1:-2]
             tr_list=list()
         except:
             print("获取数据失败")
+            traceback.print_exc()
             continue
+        fileidList=list()
         for tr in all_tr:
             d=dict()
             d['name']=tr.select('td')[2].text
             d["fileid"]=tr.select('td')[3].text
+            if(d["fileid"] in fileidList):
+                continue
             d["time"]=tr.select('td')[8].text
             d["operator"]=tr.select('td')[9].text
             d["status"]=tr.select('td')[10].text
             tr_list.append(d)
+            fileidList.append(d["fileid"])
         print(tr_list)
         try:
-            r=requests.post("http://192.168.123.219:1024/postList",json=tr_list)
+            r=requests.post("http://nas.yangning.work:1024/postList",json=tr_list)
         except:
             print("连接服务器失败")
+            traceback.print_exc()
         
